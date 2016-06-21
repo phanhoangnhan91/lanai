a39cdbe23a28dc0087a520d700e8acd9bfd2733c
Nhan Phan <nhanphan@futurify.vn>
2016-06-21 15:58:49 +0700

    [lanai-264] Add active for view by angular

--- Lanai/Lanai/Scripts/app/homeCtrl.js
+++ Lanai/Lanai/Scripts/app/homeCtrl.js
@@ -1,7 +1,8 @@
 ﻿var lanaiApp = angular.module('lanaiApp', []);
 lanaiApp.controller('homeController', ['$http', '$scope', '$rootScope',
     function ($http, $scope, $rootScope) {
-
-       
+        $scope.common = {};
+        $scope.common.activeView = 'list';
+        $scope.viewButtons = ['list','map'];
     }]);
 
