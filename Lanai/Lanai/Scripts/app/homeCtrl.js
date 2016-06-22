c5038459cbd1f6ecf0788cac0e04e413a11e165f
Nhan Phan <nhanphan@futurify.vn>
2016-06-22 08:46:02 +0700

    [Lanai-264] duplicate content by ng-repeat

--- Lanai/Lanai/Scripts/app/homeCtrl.js
+++ Lanai/Lanai/Scripts/app/homeCtrl.js
@@ -1,6 +1,10 @@
 ﻿var lanaiApp = angular.module('lanaiApp', []);
+$.getNumberArray = function (num) {
+    return (num >= 0) ? new Array(num) : [];
+}
 lanaiApp.controller('homeController', ['$http', '$scope', '$rootScope',
     function ($http, $scope, $rootScope) {
+        $scope.getNumber = $.getNumberArray;
         $scope.common = {};
         $scope.common.activeView = 'list';
         $scope.viewButtons = ['list','map'];
